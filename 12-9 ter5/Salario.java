eb86a06d6ddf146d45952dd5f99fbb600aca009b
Salva2022 <>
2023-09-12 21:19:05 -0300

    12-9 ter5

---
+++ 12-9 ter5/Salario.java
@@ -0,0 +1,13 @@
+public class Salario {
+    public static void main(String[] args) {
+        double salario = 1000;
+        double percentual = 1.5;
+        for(int ano = 2006; ano <= 2023; ano++){
+            salario = salario + (salario *(percentual/100));
+            percentual *= 2;
+            System.out.println("Ano: " + ano + "Salário: " + salario + 
+             " Percentual do ano seguinte: " + percentual);
+        }
+        System.out.println(salario);
+    }
+}
